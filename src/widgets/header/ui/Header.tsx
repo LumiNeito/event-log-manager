807f3b2bccfcbee283b9b09381cbeb300468cdd9
unknown <tuborgrosa@gmail.com>
2026-01-28 17:04:15 +0300

    fix: add white color

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -5,7 +5,7 @@ import { Header as AntHeader } from 'antd/es/layout/layout'
 export const Header = () => (
     <AntHeader>
         <Flex justify="space-between" align="center">
-            <div><b>SecurTrack</b> – Отслеживание безопасности</div>
+            <div style={{ color: '#fff' }}><b>SecurTrack</b> – Отслеживание безопасности</div>
             <LocaleSelector />
         </Flex>
     </AntHeader>
